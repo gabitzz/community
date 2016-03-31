b310a6b4831f6a06c858e0a331f0e5c15d9b3d50
gabitzz <gabitzz@gmail.com>
2016-03-31 22:13:28 +0300

    Layout changes, dialog result buttons

--- DevExpress.HybridApp.Win/Modules/Portal/FavItem.Designer.cs
+++ DevExpress.HybridApp.Win/Modules/Portal/FavItem.Designer.cs
@@ -37,6 +37,7 @@
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.S = new DevExpress.XtraLayout.EmptySpaceItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
@@ -45,6 +46,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
@@ -52,28 +54,31 @@
             this.layoutControl1.Controls.Add(this.cancelButton);
             this.layoutControl1.Controls.Add(this.okButton);
             this.layoutControl1.Controls.Add(this.textEdit1);
-            this.layoutControl1.Location = new System.Drawing.Point(-5, -3);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(171, 251, 250, 350);
             this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(646, 271);
+            this.layoutControl1.Size = new System.Drawing.Size(472, 117);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(372, 58);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(265, 58);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(262, 42);
+            this.cancelButton.Size = new System.Drawing.Size(195, 47);
             this.cancelButton.StyleController = this.layoutControl1;
             this.cancelButton.TabIndex = 7;
             this.cancelButton.Text = "No, thank you.";
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(12, 58);
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(80, 58);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(356, 42);
+            this.okButton.Size = new System.Drawing.Size(181, 47);
             this.okButton.StyleController = this.layoutControl1;
             this.okButton.TabIndex = 6;
             this.okButton.Text = "Pin it!";
@@ -82,7 +87,7 @@
             // 
             this.textEdit1.Location = new System.Drawing.Point(79, 12);
             this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(555, 42);
+            this.textEdit1.Size = new System.Drawing.Size(381, 42);
             this.textEdit1.StyleController = this.layoutControl1;
             this.textEdit1.TabIndex = 5;
             // 
@@ -93,10 +98,11 @@
             this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem2,
             this.layoutControlItem3,
-            this.layoutControlItem4});
+            this.layoutControlItem4,
+            this.S});
             this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlGroup1.Name = "Root";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(646, 271);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(472, 117);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlItem2
@@ -104,27 +110,39 @@
             this.layoutControlItem2.Control = this.textEdit1;
             this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(626, 46);
+            this.layoutControlItem2.Size = new System.Drawing.Size(452, 46);
             this.layoutControlItem2.Text = "Pin name:";
             this.layoutControlItem2.TextSize = new System.Drawing.Size(64, 20);
             // 
             // layoutControlItem3
             // 
             this.layoutControlItem3.Control = this.okButton;
-            this.layoutControlItem3.Location = new System.Drawing.Point(0, 46);
+            this.layoutControlItem3.Location = new System.Drawing.Point(68, 46);
+            this.layoutControlItem3.MinSize = new System.Drawing.Size(86, 46);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(360, 205);
+            this.layoutControlItem3.Size = new System.Drawing.Size(185, 51);
+            this.layoutControlItem3.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
             this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem3.TextVisible = false;
             // 
             // layoutControlItem4
             // 
             this.layoutControlItem4.Control = this.cancelButton;
-            this.layoutControlItem4.Location = new System.Drawing.Point(360, 46);
+            this.layoutControlItem4.Location = new System.Drawing.Point(253, 46);
+            this.layoutControlItem4.MinSize = new System.Drawing.Size(143, 46);
             this.layoutControlItem4.Name = "layoutControlItem4";
-            this.layoutControlItem4.Size = new System.Drawing.Size(266, 205);
+            this.layoutControlItem4.Size = new System.Drawing.Size(199, 51);
+            this.layoutControlItem4.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
             this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem4.TextVisible = false;
+            // 
+            // S
+            // 
+            this.S.AllowHotTrack = false;
+            this.S.Location = new System.Drawing.Point(0, 46);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(68, 51);
+            this.S.TextSize = new System.Drawing.Size(0, 0);
             // 
             // contextMenuStrip1
             // 
@@ -137,7 +155,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.layoutControl1);
             this.Name = "FavItem";
-            this.Size = new System.Drawing.Size(642, 288);
+            this.Size = new System.Drawing.Size(472, 117);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
@@ -145,6 +163,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -160,5 +179,6 @@
         private XtraLayout.LayoutControlItem layoutControlItem3;
         private XtraLayout.LayoutControlItem layoutControlItem4;
         private XtraEditors.TextEdit textEdit1;
+        private XtraLayout.EmptySpaceItem S;
     }
 }
